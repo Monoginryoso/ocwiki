0381c5b51b252678f2a26c9628b395b659465691
Monoginryoso <ghostpipe611@tutanota.com>
2023-07-08 21:20:17 +0800

    fix the 404

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -11,7 +11,7 @@ const config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'monoginryoso.github.io/ocwiki/',
+  url: 'https://monoginryoso.github.io/',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/ocwiki/',
